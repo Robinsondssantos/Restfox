81e0ff6afee06ee48ac1e2d7912332e04a699ef9
Robinson D. S. Santos <robinsondssantos@gmail.com>
2024-03-12 16:11:13 -0300

    refactor AddSocketModal - use script setup

--- packages/ui/src/components/modals/AddSocketModal.vue
+++ packages/ui/src/components/modals/AddSocketModal.vue
@@ -13,59 +13,54 @@
     </form>
 </template>
 
-<script>
+<script setup>
+import { ref, computed, watch } from 'vue'
+import { useStore } from 'vuex'
 import Modal from '@/components/Modal.vue'
 
-export default {
-    directives: {
-        focus: {
-            mounted(element) {
-                element.focus()
-                element.select()
-            }
-        }
-    },
-    props: {
-        showModal: Boolean,
-        parentId: {
-            type: String,
-            default: null
-        }
-    },
-    components: {
-        Modal
-    },
-    data() {
-        return {
-            socketName: 'New Socket',
-        }
-    },
-    computed: {
-        showModalComp: {
-            get() {
-                return this.showModal
-            },
-            set(value) {
-                this.$emit('update:showModal', value)
-            }
-        }
-    },
-    watch: {
-        showModal() {
-            if (this.showModal) {
-                this.socketName = 'New Socket'
-            }
-        }
-    },
-    methods: {
-        async createSocket() {
-            this.$store.dispatch('createCollectionItem', {
-                type: 'socket',
-                name: this.socketName,
-                parentId: this.parentId
-            })
-            this.showModalComp = false
-        }
-    },
+const vFocus = {
+    mounted: (el) => {
+        el.focus()
+        el.select()
+    }
 }
+
+const props = defineProps({
+    showModal: Boolean,
+    parentId: {
+        type: String,
+        default: null
+    }
+})
+
+const emit = defineEmits(['update:showModal'])
+
+const socketName = ref('New Socket')
+
+const store = useStore()
+
+const showModalComp = computed({
+    get() {
+        return props.showModal
+    },
+    set(value) {
+        emit('update:showModal', value)
+    }
+})
+
+watch(props.showModal, (v) => {
+    if (v) {
+        socketName.value = 'New Socket'
+    }
+})
+
+async function createSocket() {
+    store.dispatch('createCollectionItem', {
+        type: 'socket',
+        name: socketName.value,
+        parentId: props.parentId
+    })
+    showModalComp.value = false
+}
+
 </script>
