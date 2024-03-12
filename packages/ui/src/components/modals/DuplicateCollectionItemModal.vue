c305ca0e4c85811b9ec789ed9cfe9a91f7610fb3
Robinson D. S. Santos <robinsondssantos@gmail.com>
2024-03-12 17:11:20 -0300

    refactor DuplicateCollectionItemModal - use script setup

--- packages/ui/src/components/modals/DuplicateCollectionItemModal.vue
+++ packages/ui/src/components/modals/DuplicateCollectionItemModal.vue
@@ -13,84 +13,81 @@
     </form>
 </template>
 
-<script>
+<script setup>
+import { ref, computed, watch } from 'vue'
+import { useStore } from 'vuex'
 import Modal from '@/components/Modal.vue'
 
-export default {
-    directives: {
-        focus: {
-            mounted(element) {
-                element.focus()
-                element.select()
-            }
-        }
-    },
-    props: {
-        showModal: Boolean,
-        collectionItemToDuplicate: Object
-    },
-    components: {
-        Modal
-    },
-    data() {
-        return {
-            newName: ''
-        }
-    },
-    computed: {
-        showModalComp: {
-            get() {
-                return this.showModal
-            },
-            set(value) {
-                this.$emit('update:showModal', value)
-            }
-        },
-        title() {
-            if(this.collectionItemToDuplicate._type === 'request_group') {
-                return 'Duplicate Folder'
-            }
-
-            if(this.collectionItemToDuplicate._type === 'request') {
-                return 'Duplicate Request'
-            }
-
-            if(this.collectionItemToDuplicate._type === 'socket') {
-                return 'Duplicate Socket'
-            }
-
-            return ''
-        },
-        placeholder() {
-            if(this.collectionItemToDuplicate._type === 'request_group') {
-                return 'New Folder Name'
-            }
-
-            if(this.collectionItemToDuplicate._type === 'request') {
-                return 'New Request Name'
-            }
-
-            if(this.collectionItemToDuplicate._type === 'socket') {
-                return 'New Socket Name'
-            }
-
-            return ''
-        }
-    },
-    watch: {
-        collectionItemToDuplicate() {
-            if(this.collectionItemToDuplicate) {
-                this.newName = this.collectionItemToDuplicate.name
-            }
-        }
-    },
-    methods: {
-        async duplicateCollectionItem() {
-            const newCollectionItem = JSON.parse(JSON.stringify(this.collectionItemToDuplicate))
-            newCollectionItem.name = this.newName
-            this.$store.dispatch('duplicateCollectionItem', newCollectionItem)
-            this.showModalComp = false
-        }
+const vFocus = {
+    mounted: (el) => {
+        el.focus()
+        el.select()
     }
 }
+
+const props = defineProps({
+    showModal: Boolean,
+    collectionItemToDuplicate: Object
+})
+
+const emit = defineEmits(['update:showModal'])
+
+const newName = ref('')
+
+const store = useStore()
+
+const showModalComp = computed({
+    get() {
+        return props.showModal
+    },
+    set(value) {
+        emit('update:showModal', value)
+    }
+})
+
+const title = computed(() => {
+    if (props.collectionItemToDuplicate._type === 'request_group') {
+        return 'Duplicate Folder'
+    }
+
+    if (props.collectionItemToDuplicate._type === 'request') {
+        return 'Duplicate Request'
+    }
+
+    if (props.collectionItemToDuplicate._type === 'socket') {
+        return 'Duplicate Socket'
+    }
+
+    return ''
+})
+
+const placeholder = computed(() => {
+    if (props.collectionItemToDuplicate._type === 'request_group') {
+        return 'New Folder Name'
+    }
+
+    if (props.collectionItemToDuplicate._type === 'request') {
+        return 'New Request Name'
+    }
+
+    if (props.collectionItemToDuplicate._type === 'socket') {
+        return 'New Socket Name'
+    }
+
+    return ''
+})
+
+watch(props.collectionItemToDuplicate, (v) => {
+    if (v) {
+        newName.value = props.collectionItemToDuplicate.name
+    }
+})
+
+async function duplicateCollectionItem() {
+    const newCollectionItem = JSON.parse(JSON.stringify(props.collectionItemToDuplicate))
+    newCollectionItem.name = newName.value
+    store.dispatch('duplicateCollectionItem', newCollectionItem)
+    showModalComp.value = false
+}
+
 </script>
